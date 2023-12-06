e8fa889eb079d13d3b4ddf64f7119228060d6621
Josip Filipovic <josipfilipovic99@gmail.com>
2023-12-06 12:22:30 +0100

    feat: :art: Adedd login page

---
+++ src/views/login/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title> This is login page</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <h1>Log In</h1>
+            <ion-list lines="none">
+                <ion-item>
+                    <ion-input
+                      label-placement="floating"
+                      value="example@gmai.com"  
+                    >
+                        <div slot="label">Email <ion-text color="danger">(Required)</ion-text></div>
+                    </ion-input>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonContent, IonToolbar, IonTitle, IonList, IonItem, IonInput, IonText,} from '@ionic/vue';
+</script>
+
+<style scoped>
+ion-input {
+    border: solid 1px black;
+    border-radius: 10px;
+    margin: 10px;
+}
+</style>
---
+++ src/views/tabs/Tab1Page.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+        <ion-button router-link="/login">Login</ion-button>
+      </ion-header>
+      <ion-item>
+                    <ion-input
+                      label-placement="floating"
+                      value="example@gmai.com"  
+                    >
+                        <div slot="label">Email <ion-text color="danger">(Required)</ion-text></div>
+                    </ion-input>
+                </ion-item>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, 
+         IonHeader, 
+         IonToolbar, 
+         IonTitle, 
+         IonContent, 
+         IonButton,
+         IonItem,
+         IonInput,
+         IonText
+        } from '@ionic/vue';
+
+
+</script>
